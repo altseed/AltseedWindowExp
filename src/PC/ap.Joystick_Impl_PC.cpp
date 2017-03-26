8b2169f99ad404588759f5b3651b7f9dfaf92db7
durswd <swda.durl@gmail.com>
2017-03-26 14:51:51 +0900

    ジョイステイック途中まで

--- src/PC/ap.Joystick_Impl_PC.cpp
+++ src/PC/ap.Joystick_Impl_PC.cpp
@@ -6,12 +6,74 @@ namespace ap
 
 Joystick_Impl_PC::Joystick_Impl_PC(Window* window)
 {
+	isPresent.fill(false);
+	buttonCount.fill(0);
+	axisCount.fill(0);
 
+	for (int32_t i = 0; i < MAX_JOYSTICKS_NUM; i++)
+	{
+		currentHit[i].fill(false);
+		preHit[i].fill(false);
+		currentAxis[i].fill(0);
+	}
+
+	for (int i = 0; i < MAX_JOYSTICKS_NUM; i++)
+	{
+		isPresent[i] = glfwJoystickPresent(i) == GLFW_TRUE;
+	}
 }
 
 Joystick_Impl_PC::~Joystick_Impl_PC()
 {
 
+}
+
+void Joystick_Impl_PC::RefreshInputState()
+{
+	preHit = currentHit;
+
+	for (int jind = 0; jind < MAX_JOYSTICKS_NUM; jind++)
+	{
+		int32_t axesCount = 0;
+		auto ax = glfwGetJoystickAxes(jind, &axesCount);
+		for (int i = 0; i < axesCount; ++i)
+		{
+			currentAxis[jind][i] = ax[i];
+		}
+
+		int32_t buttonsCount = 0;
+		auto btns = glfwGetJoystickButtons(jind, &buttonsCount);
+
+		for (int i = 0; i < buttonsCount; ++i)
+		{
+			currentHit[jind][i] = (bool)btns[i];
+		}
+
+		buttonCount[jind] = buttonsCount;
+		axisCount[jind] = axesCount;
+	}
+}
+
+
+bool Joystick_Impl_PC::IsPresent(int32_t joystickIndex)
+{
+	if (joystickIndex < 0) return false;
+	if (joystickIndex >= MAX_JOYSTICKS_NUM) return false;
+
+	return isPresent[joystickIndex];
+}
+
+InputState Joystick_Impl_PC::GetButtonState(int32_t joystickIndex, int32_t buttonIndex) const
+{
+	if (currentHit[joystickIndex][buttonIndex] && preHit[joystickIndex][buttonIndex]) return InputState::Hold;
+	else if (!currentHit[joystickIndex][buttonIndex] && preHit[joystickIndex][buttonIndex]) return InputState::Release;
+	else if (currentHit[joystickIndex][buttonIndex] && !preHit[joystickIndex][buttonIndex]) return InputState::Push;
+	else return InputState::Free;
+}
+
+float Joystick_Impl_PC::GetAxisState(int32_t joystickIndex, int32_t axisIndex) const
+{
+	return currentAxis[joystickIndex][axisIndex];
 }
 
 }
--- src/PC/ap.Joystick_Impl_PC.h
+++ src/PC/ap.Joystick_Impl_PC.h
@@ -9,11 +9,33 @@ namespace ap
 		: public Joystick
 	{
 	private:
+		static const int MAX_AXES_NUM = 10;
+		static const int MAX_BUTTONS_NUM = 30;
+		static const int MAX_JOYSTICKS_NUM = 16;
+
 		Window*		window = nullptr;
+
+		std::array<bool, MAX_JOYSTICKS_NUM>			isPresent;
+
+		std::array<int32_t, MAX_JOYSTICKS_NUM>			buttonCount;
+		std::array<int32_t, MAX_JOYSTICKS_NUM>			axisCount;
+
+		std::array<std::array<bool,MAX_BUTTONS_NUM>, MAX_JOYSTICKS_NUM>	currentHit;
+		std::array<std::array<bool,MAX_BUTTONS_NUM>, MAX_JOYSTICKS_NUM>	preHit;
+
+		std::array<std::array<bool, MAX_AXES_NUM>, MAX_JOYSTICKS_NUM>	currentAxis;
 
 	public:
 		Joystick_Impl_PC(Window* window);
 		virtual ~Joystick_Impl_PC();
+
+		void RefreshInputState();
+
+		bool IsPresent(int32_t joystickIndex);
+
+		InputState GetButtonState(int32_t joystickIndex, int32_t buttonIndex) const;
+
+		float GetAxisState(int32_t joystickIndex, int32_t axisIndex) const;
 	};
 
 }
