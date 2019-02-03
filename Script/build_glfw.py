1d5567176c50ba1e1abf6ca5becb6c6d6c3e9b02
GON <hutabasuzukiryu@live.jp>
2019-02-03 19:36:40 +0900

    update script

--- Script/build_glfw.py
+++ Script/build_glfw.py
@@ -11,60 +11,73 @@ aceutils.mkdir(r"../lib/")
 
 with aceutils.CurrentDir('../Temp'):
 
-	# Copy from submodule
-	aceutils.copytree('../Downloads/glfw/','./glfw/', True)
-	aceutils.rm('./glfw/.git')
-	aceutils.rmdir('./glfw/.github/')
+    # Copy from submodule
+    aceutils.copytree('../Downloads/glfw/', './glfw/', True)
+    aceutils.rm('./glfw/.git')
+    aceutils.rmdir('./glfw/.github/')
 
-	# Compile
-	aceutils.rmdir(r"glfw_bin")
-	aceutils.rmdir(r"glfw_bin_x64")
+    # Compile
+    aceutils.rmdir(r"glfw_bin")
+    aceutils.rmdir(r"glfw_bin_x64")
 
-	aceutils.editCmakeForACE(pathname + r'/CMakeLists.txt')
-	aceutils.mkdir(r"glfw_bin")
-	aceutils.mkdir(r"glfw_bin_x64")
+    aceutils.editCmakeForACE(pathname + r'/CMakeLists.txt')
+    aceutils.mkdir(r"glfw_bin")
+    aceutils.mkdir(r"glfw_bin_x64")
 
-	with aceutils.CurrentDir('glfw_bin'):
-		if aceutils.isWin():
-			aceutils.call(aceutils.cmd_cmake+r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF -D USE_INTERNAL_LOADER:BOOL=OFF ../' + pathname + '/')
-			aceutils.call(aceutils.cmd_compile + r'ALL_BUILD.vcxproj /p:configuration=Debug')
-			aceutils.call(aceutils.cmd_compile + r'ALL_BUILD.vcxproj /p:configuration=Release')
-		elif aceutils.isMac():
-			aceutils.call(r'cmake -G "Unix Makefiles" -D BUILD_SHARED_LIBS:BOOL=OFF -D CMAKE_INSTALL_PREFIX:PATH=../.. "-DCMAKE_OSX_ARCHITECTURES=x86_64;i386" ../' + pathname + '/')
-			aceutils.call(r'make install')
-		else:
-			aceutils.call(r'cmake -G "Unix Makefiles" -D BUILD_SHARED_LIBS:BOOL=OFF -D CMAKE_INSTALL_PREFIX=../.. ../' + pathname + '/')
-			aceutils.call(r'make install')
+    with aceutils.CurrentDir('glfw_bin'):
+        if aceutils.isWin():
+            aceutils.call(
+                aceutils.cmd_cmake+r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF -D USE_INTERNAL_LOADER:BOOL=OFF ../' + pathname + '/')
+            aceutils.call(aceutils.cmd_compile +
+                          r'ALL_BUILD.vcxproj /p:configuration=Debug')
+            aceutils.call(aceutils.cmd_compile +
+                          r'ALL_BUILD.vcxproj /p:configuration=Release')
+        elif aceutils.isMac():
+            aceutils.call(r'cmake -G "Unix Makefiles" -D BUILD_SHARED_LIBS:BOOL=OFF -D CMAKE_INSTALL_PREFIX:PATH=../.. "-DCMAKE_OSX_ARCHITECTURES=x86_64' +
+                          (';i386' if aceutils.Isi386() else '') + r'" ../' + pathname + '/')
+            aceutils.call(r'make install')
+        else:
+            aceutils.call(
+                r'cmake -G "Unix Makefiles" -D BUILD_SHARED_LIBS:BOOL=OFF -D CMAKE_INSTALL_PREFIX=../.. ../' + pathname + '/')
+            aceutils.call(r'make install')
 
-	with aceutils.CurrentDir('glfw_bin_x64'):
-		if aceutils.isWin():
-			aceutils.call(aceutils.cmd_cmake_x64+r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF ../' + pathname + '/')
-			aceutils.call(aceutils.cmd_compile + r'ALL_BUILD.vcxproj /p:configuration=Debug')
-			aceutils.call(aceutils.cmd_compile + r'ALL_BUILD.vcxproj /p:configuration=Release')
-	
-	aceutils.mkdir(r"../include/GLFW/")
+    with aceutils.CurrentDir('glfw_bin_x64'):
+        if aceutils.isWin():
+            aceutils.call(aceutils.cmd_cmake_x64 +
+                          r'-D USE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=OFF ../' + pathname + '/')
+            aceutils.call(aceutils.cmd_compile +
+                          r'ALL_BUILD.vcxproj /p:configuration=Debug')
+            aceutils.call(aceutils.cmd_compile +
+                          r'ALL_BUILD.vcxproj /p:configuration=Release')
 
-	if aceutils.isWin():
+    aceutils.mkdir(r"../include/GLFW/")
 
-		aceutils.mkdir(r'../lib/x86/')
-		aceutils.mkdir(r'../lib/x86/Debug')
-		aceutils.mkdir(r'../lib/x86/Release')
+    if aceutils.isWin():
 
-		aceutils.mkdir(r'../lib/x64/')
-		aceutils.mkdir(r'../lib/x64/Debug')
-		aceutils.mkdir(r'../lib/x64/Release')
+        aceutils.mkdir(r'../lib/x86/')
+        aceutils.mkdir(r'../lib/x86/Debug')
+        aceutils.mkdir(r'../lib/x86/Release')
 
-		aceutils.copy(pathname + r'/include/GLFW/glfw3.h', r'../include/GLFW/')
-		aceutils.copy(pathname + r'/include/GLFW/glfw3native.h', r'../include/GLFW')
+        aceutils.mkdir(r'../lib/x64/')
+        aceutils.mkdir(r'../lib/x64/Debug')
+        aceutils.mkdir(r'../lib/x64/Release')
 
-		aceutils.copy(r'glfw_bin/src/Debug/glfw3.lib', r'../lib/x86/Debug/')
-		aceutils.copy(r'glfw_bin/src/Release/glfw3.lib', r'../lib/x86/Release/')
+        aceutils.copy(pathname + r'/include/GLFW/glfw3.h', r'../include/GLFW/')
+        aceutils.copy(pathname + r'/include/GLFW/glfw3native.h',
+                      r'../include/GLFW')
 
-		aceutils.copy(r'glfw_bin_x64/src/Debug/glfw3.lib', r'../lib/x64/Debug/')
-		aceutils.copy(r'glfw_bin_x64/src/Release/glfw3.lib', r'../lib/x64/Release/')
+        aceutils.copy(r'glfw_bin/src/Debug/glfw3.lib', r'../lib/x86/Debug/')
+        aceutils.copy(r'glfw_bin/src/Release/glfw3.lib',
+                      r'../lib/x86/Release/')
 
-	else:
-		aceutils.copy(pathname + r'/include/GLFW/glfw3.h', r'../include/GLFW/')
-		aceutils.copy(pathname + r'/include/GLFW/glfw3native.h', r'../include/GLFW')
+        aceutils.copy(r'glfw_bin_x64/src/Debug/glfw3.lib',
+                      r'../lib/x64/Debug/')
+        aceutils.copy(r'glfw_bin_x64/src/Release/glfw3.lib',
+                      r'../lib/x64/Release/')
 
-		aceutils.copy(r'glfw_bin/src/libglfw3.a', r'../lib')
+    else:
+        aceutils.copy(pathname + r'/include/GLFW/glfw3.h', r'../include/GLFW/')
+        aceutils.copy(pathname + r'/include/GLFW/glfw3native.h',
+                      r'../include/GLFW')
+
+        aceutils.copy(r'glfw_bin/src/libglfw3.a', r'../lib')
